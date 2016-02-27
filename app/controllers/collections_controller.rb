cd71acddf1e3553ae9aa3625435070479141400e
System Administrator <root@ip-192-168-2-71.ec2.internal>
2016-02-27 11:16:50 -0700

    helpful notes on working with product variants

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,8 +1,20 @@
+require 'json'
+
 class CollectionsController < AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
 
     @variants = ShopifyAPI::Variant.find(:all, :params => {:limit => 10})
+
+    # puts type(@variants)
+
+
+
+    # the field that displays the value is _
+    # but the field that displays the key is _, and this is what we want to group by
+
+    # test this out
+
 
   end
 
