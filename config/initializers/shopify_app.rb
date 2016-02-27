a4e245af8d595114755b6de7fe6482ba7ee0fe9c
System Administrator <root@ip-192-168-2-71.ec2.internal>
2016-02-27 09:22:55 -0700

    initial commit shopify color selector

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+  config.api_key = "4ce94bba5fd533cb93c0ee04e45fbd0a"
+  config.secret = "e598e4031e12a42377973e02e3d87549"
+  config.redirect_uri = "http://localhost:3000/auth/shopify/callback"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
